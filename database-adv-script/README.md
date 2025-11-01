758e0ff7e18a679e10568ed3bafd18aadec3eeb0
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-01 20:40:12 +0100

    Add join queries

--- database-adv-script/README.md
+++ database-adv-script/README.md
@@ -4,3 +4,8 @@
 ### Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 
 ### Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+### The first query finds properties by checking if their property_id appears in a subquery that groups reviews by property and computes an average rating exceeding 4.0.
+
+
+### The second query relates each user in the outer query with their bookings in the correlated subquery that counts bookings per user, returning those with more than three bookings.
--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -56,3 +56,26 @@ FULL OUTER JOIN
 ON 
     u.user_id = b.user_id;
 
+--The first query finds properties by checking if their property_id appears in a subquery
+-- that groups reviews by property and computes an average rating exceeding 4.0.
+
+SELECT property_id, name
+FROM Property
+WHERE property_id IN (
+  SELECT property_id
+  FROM Review
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+);
+
+--The second query relates each user in the outer query with their bookings in the 
+--correlated subquery that counts bookings per user, returning those with more than 
+--three bookings.
+SELECT user_id, first_name, last_name
+FROM User u
+WHERE (
+  SELECT COUNT(*)
+  FROM Booking b
+  WHERE b.user_id = u.user_id
+) > 3;
+
