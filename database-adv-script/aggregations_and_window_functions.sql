aeb4970698e30b1d6d00ad02aff6ef1bc8f7abae
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-05 04:04:07 +0100

    Add aggregations

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -31,3 +31,18 @@ GROUP BY
     p.property_id, p.name
 ORDER BY 
     property_rank ASC;
+
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+FROM 
+    Property AS p
+LEFT JOIN 
+    Booking AS b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name
+ORDER BY 
+    property_rank ASC;
