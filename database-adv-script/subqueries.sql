bafb1f4ba685f93bf8b080b495b880a11d9cdb76
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2025-11-05 04:00:26 +0100

    Add sub queries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -6,3 +6,12 @@ WHERE property_id IN (
     GROUP BY property_id
     HAVING AVG(rating) > 4.0
 );
+
+
+SELECT u.user_id, u.username
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
